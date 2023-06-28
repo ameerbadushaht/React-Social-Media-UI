31e1ebc3db0090b19cdb4254cbcbbd5131244446
Ameer Badushah T <84665215+ameerbadushaht@users.noreply.github.com>
2023-06-28 20:59:41 +0530

    Add files via upload

---
+++ src/components/Topbar.jsx
@@ -0,0 +1,71 @@
+
+// import { BrowserRouter as Router } from 'react-router-dom';
+import SearchIcon from "@mui/icons-material/Search";
+import PersonIcon from "@mui/icons-material/Person";
+import { Link } from 'react-router-dom';
+
+import ChatIcon from "@mui/icons-material/Chat";
+import NotificationsIcon from "@mui/icons-material/Notifications";
+// import { Dropdown } from 'rsuite';
+
+import { useState } from "react";
+
+export default function Topbar({onShowRegister}) {
+  const [isClicked, setClicked] = useState(false);
+
+  function clicked() {
+    setClicked(!isClicked);
+  }
+
+  function imageClick() {
+    console.log("image clicked");
+  }
+
+  return (
+    <div className="topbarContainer">
+      <div className="topbarCenter">
+        <div className="searchbar">
+          <SearchIcon onClick={clicked} className="searchIcon" />
+          {isClicked ? (
+            <input
+              placeholder="Search for friend, post or video"
+              className="searchInput"
+            />
+          ) : null}
+        </div>
+      </div>
+      <div className="topbarLeft">
+        <span className="logo">YouvA</span>
+      </div>
+
+      <div className="topbarRight">
+        <div className="topbarLinks">
+          <span className="topbarLink"><Link to="/">Home</Link></span>
+          <span className="topbarLink">Timeline</span>
+         <span> <Link to="/">Home</Link> </span>
+         <span> <Link to="/Profile" >Profile</Link> </span>
+        </div>
+        <div className="topbarIcons">
+          <div className="topbarIconItem">
+            <PersonIcon />
+            <span className="topbarIconBadge">1</span>
+          </div>
+          <div className="topbarIconItem">
+            <ChatIcon />
+            <span className="topbarIconBadge">2</span>
+          </div>
+          <div className="topbarIconItem">
+            <NotificationsIcon />
+            <span className="topbarIconBadge">1</span>
+          </div>
+        </div>
+        <img
+          onClick={imageClick}
+          src="/assets/person/1.jpeg"
+          alt=""
+          className="topbarImg"
+        />
+      </div>
+    </div>
+  );
+}
